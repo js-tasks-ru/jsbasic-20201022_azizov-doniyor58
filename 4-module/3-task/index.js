1f122e31b35b70d4798da27923db082da9d408ac
Azizov Doniyor Alisherovich <DAAzizov1@sbebank.ru>
2020-10-30 01:37:45 +0300

    Занятие 4: Знакомство с DOM / Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,28 @@
  * @param {Element} table
  */
 function highlight(table) {
+    const arrayOfRows = [...table.tBodies[0].rows]
+
+    const AGE = 1
+    const GENDER = 2
+    const STATUS = 3
+
+    arrayOfRows.forEach(({ cells }) => {
+        cells[STATUS].hasAttribute('data-available')
+            ? cells[STATUS].dataset.available === 'true'
+                ? cells[STATUS].closest('tr').classList.add('available')
+                : cells[STATUS].closest('tr').classList.add('unavailable')
+
+            : cells[STATUS].closest('tr').setAttribute('hidden', '')
+
+
+        cells[GENDER].innerHTML === 'm'
+            ? cells[GENDER].closest('tr').classList.add('male')
+            : cells[GENDER].closest('tr').classList.add('female')
+
+
+        Number(cells[AGE].innerHTML) < 18
+        ?  cells[AGE].closest('tr').style.textDecoration = 'line-through'
+        : null
+    })
 }
