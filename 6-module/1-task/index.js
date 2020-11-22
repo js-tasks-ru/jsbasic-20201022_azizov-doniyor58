a8f049d4e1f00445f2de540598af3680bcdf9a5e
Doniyor Azizov <d.azizov@paymo.ru>
2020-11-22 17:15:12 +0300

    Занятие 6: JS классы и компоненты / Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,55 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table')
+
+    const thead =
+    `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>`
+
+    const tableInner = rows
+      .map((row)=> {
+        const rowValues = Object.values(row)
+
+        const rowDatas = rowValues
+          .map((value) => `<td>${value}</td>`)
+          .join('')
+
+        return `
+          <tr>
+            ${rowDatas}
+            <td><button>X</button></td>
+          </tr>
+          `
+      })
+      .join('')
+
+    const tbody = `
+      <tbody>
+        ${tableInner}
+      </tbody>
+    `
+
+    this.elem.innerHTML = `
+      ${thead}
+      ${tbody}
+    `
+
+    this.elem.addEventListener('click', (e) => this.onClick(e))
+  }
+
+  onClick(e) {
+    if (e.target.tagName !== 'BUTTON') return
+
+    const tr = e.target.closest('tr')
+
+    tr.remove()
   }
 }
